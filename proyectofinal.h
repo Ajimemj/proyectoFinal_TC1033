48b637eaa0dc05771a7bb199b33296111c58a309
Ajimemj <A01800234@tec.mx>
2023-06-12 17:52:42 -0600

    Create proyectofinal.h

---
+++ proyectofinal.h
@@ -0,0 +1,28 @@
+/*
+* Autor: Jimena Mendoza Jasso
+A01800234
+Situación problema. "Automovil"
+TC1033. Pensamiento computacional orientado a objetos
+*/
+class automovil{
+
+private:
+bool encendido_apagado;
+int velocidad;
+int velocidad_max;
+
+public:
+automovil(){ 
+encendido_apagado = false;
+velocidad = 0;
+velocidad_max = 150;
+}
+
+};
+class tablero{
+
+
+
+
+
+};
